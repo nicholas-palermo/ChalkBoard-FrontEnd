4941f6156319fa6de733a0b9e5d56db02598f98c
James Lopez <lopez.jamesrussel@gmail.com>
2022-01-25 17:46:33 -0500

    Added Responsiveness

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,25 +10,27 @@ function Login() {
         <div>
 
             <center>
-                <div className=" w-50 rounded-top">
+                <div className="container cw-50 rounded-top">
                     <label className="form-label mt-4"><h1 className="title "><strong>Chalkboard</strong> Login</h1></label><br />
 
                 </div>
                 <div className="form-group ">
 
                     <div className="credentials bg-primary w-50 rounded p-2">
-                        <div className="form-floating p-1">
-                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                            <label htmlFor="floatingInput" className="ps-3">Email</label>
-                            <sub className="text-light">ex.John.Doe29@login.chalkboard.edu</sub>
-                        </div>
-                        <div className="form-floating p-1">
-                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                            <label htmlFor="floatingPassword" className="ps-3">Password</label>
-                            <sub className="text-light">ex.Doe&John</sub>
-                        </div>
+                        <form>
+                            <div className="form-floating p-1">
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label htmlFor="floatingInput" className="ps-3">Email</label>
+                                <sub className="text-light">ex.John.Doe29@login.chalkboard.edu</sub>
+                            </div>
+                            <div className="form-floating p-1">
+                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                                <label htmlFor="floatingPassword" className="ps-3">Password</label>
+                                <sub className="text-light">ex.Doe&John</sub>
+                            </div>
+                        </form>
                         <div className="buttonContainer mt-2">
-                            <button type="button" className="btn btn-lg btn-dark ">Login</button>
+                            <button type="button" className="btn btn-lg btn-dark">Login</button>
                             <div className="support-links m-3 p-2">
                                 <Link to="/newUser" className="text-light">New User</Link>
                                 <span className="seperator m-1">|</span>
